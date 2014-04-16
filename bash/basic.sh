36a555615dfb648da259990e5b6065518015c58b
yipeng <yipeng@ganji.com>
2014-04-16 16:36:30 +0800

    if expression

--- bash/basic.sh
+++ bash/basic.sh
@@ -4,3 +4,11 @@
 var="a string variable"   #   define a variable
 echo $var #   use a varibale
 
+magic=99
+
+read guess
+if [ $guess -eq 99 ]; then
+    echo 'it is magical!'
+else
+    echo 'just so so'
+fi
