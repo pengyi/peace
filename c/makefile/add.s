9d29447c92ed5d228279609965e9e280d5bb87f4
yipeng <yipeng@ganji.com>
2014-04-22 16:05:38 +0800

    makefile

---
+++ c/makefile/add.s
@@ -0,0 +1,25 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_add
+	.align	4, 0x90
+_add:                                   ## @add
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp2:
+	.cfi_def_cfa_offset 16
+Ltmp3:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp4:
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movl	%esi, %eax
+	popq	%rbp
+	ret
+	.cfi_endproc
+
+
+.subsections_via_symbols
